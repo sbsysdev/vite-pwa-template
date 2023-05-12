4fdd01319af3693b27e080c65ccc106f3e62351d
Steven Bustillo <sb.sys.dev@gmail.com>
2023-05-12 13:54:40 -0600

    :lipstick: - added loader

--- src/shared/components/Loader/Loader.tsx
+++ src/shared/components/Loader/Loader.tsx
@@ -6,53 +6,121 @@ const Loader = memo(() => {
         <div className="fixed inset-0 p-4 z-[200] flex flex-col justify-center items-center">
             <div className="absolute inset-0 bg-dark-900 bg-opacity-75" />
 
-            <span className="absolute flex flex-col justify-center items-center w-24 text-primary-500">
-                <svg viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg" stroke="currentColor">
-                    <g fill="none" fillRule="evenodd" strokeWidth="2">
-                        <circle cx="22" cy="22" r="1">
-                            <animate
-                                attributeName="r"
-                                begin="0s"
-                                dur="1.8s"
-                                values="1; 20"
-                                calcMode="spline"
-                                keyTimes="0; 1"
-                                keySplines="0.165, 0.84, 0.44, 1"
-                                repeatCount="indefinite"
-                            />
-                            <animate
-                                attributeName="stroke-opacity"
-                                begin="0s"
-                                dur="1.8s"
-                                values="1; 0"
-                                calcMode="spline"
-                                keyTimes="0; 1"
-                                keySplines="0.3, 0.61, 0.355, 1"
-                                repeatCount="indefinite"
-                            />
-                        </circle>
-                        <circle cx="22" cy="22" r="1">
-                            <animate
-                                attributeName="r"
-                                begin="-0.9s"
-                                dur="1.8s"
-                                values="1; 20"
-                                calcMode="spline"
-                                keyTimes="0; 1"
-                                keySplines="0.165, 0.84, 0.44, 1"
-                                repeatCount="indefinite"
-                            />
-                            <animate
-                                attributeName="stroke-opacity"
-                                begin="-0.9s"
-                                dur="1.8s"
-                                values="1; 0"
-                                calcMode="spline"
-                                keyTimes="0; 1"
-                                keySplines="0.3, 0.61, 0.355, 1"
-                                repeatCount="indefinite"
-                            />
-                        </circle>
+            <span className="absolute flex flex-col justify-center items-center w-24 text-secondary-500">
+                <svg viewBox="0 0 58 58" xmlns="http://www.w3.org/2000/svg">
+                    <g fill="none" fillRule="evenodd">
+                        <g transform="translate(2 1)" stroke="currentColor" strokeWidth="1.0">
+                            <circle
+                                cx="42.601"
+                                cy="11.462"
+                                r="5"
+                                fillOpacity="1"
+                                fill="currentColor">
+                                <animate
+                                    attributeName="fill-opacity"
+                                    begin="0s"
+                                    dur="1.3s"
+                                    values="1;0;0;0;0;0;0;0"
+                                    calcMode="linear"
+                                    repeatCount="indefinite"
+                                />
+                            </circle>
+                            <circle
+                                cx="49.063"
+                                cy="27.063"
+                                r="5"
+                                fillOpacity="0"
+                                fill="currentColor">
+                                <animate
+                                    attributeName="fill-opacity"
+                                    begin="0s"
+                                    dur="1.3s"
+                                    values="0;1;0;0;0;0;0;0"
+                                    calcMode="linear"
+                                    repeatCount="indefinite"
+                                />
+                            </circle>
+                            <circle
+                                cx="42.601"
+                                cy="42.663"
+                                r="5"
+                                fillOpacity="0"
+                                fill="currentColor">
+                                <animate
+                                    attributeName="fill-opacity"
+                                    begin="0s"
+                                    dur="1.3s"
+                                    values="0;0;1;0;0;0;0;0"
+                                    calcMode="linear"
+                                    repeatCount="indefinite"
+                                />
+                            </circle>
+                            <circle cx="27" cy="49.125" r="5" fillOpacity="0" fill="currentColor">
+                                <animate
+                                    attributeName="fill-opacity"
+                                    begin="0s"
+                                    dur="1.3s"
+                                    values="0;0;0;1;0;0;0;0"
+                                    calcMode="linear"
+                                    repeatCount="indefinite"
+                                />
+                            </circle>
+                            <circle
+                                cx="11.399"
+                                cy="42.663"
+                                r="5"
+                                fillOpacity="0"
+                                fill="currentColor">
+                                <animate
+                                    attributeName="fill-opacity"
+                                    begin="0s"
+                                    dur="1.3s"
+                                    values="0;0;0;0;1;0;0;0"
+                                    calcMode="linear"
+                                    repeatCount="indefinite"
+                                />
+                            </circle>
+                            <circle
+                                cx="4.938"
+                                cy="27.063"
+                                r="5"
+                                fillOpacity="0"
+                                fill="currentColor">
+                                <animate
+                                    attributeName="fill-opacity"
+                                    begin="0s"
+                                    dur="1.3s"
+                                    values="0;0;0;0;0;1;0;0"
+                                    calcMode="linear"
+                                    repeatCount="indefinite"
+                                />
+                            </circle>
+                            <circle
+                                cx="11.399"
+                                cy="11.462"
+                                r="5"
+                                fillOpacity="0"
+                                fill="currentColor">
+                                <animate
+                                    attributeName="fill-opacity"
+                                    begin="0s"
+                                    dur="1.3s"
+                                    values="0;0;0;0;0;0;1;0"
+                                    calcMode="linear"
+                                    repeatCount="indefinite"
+                                />
+                            </circle>
+                            <circle cx="27" cy="5" r="5" fillOpacity="0" fill="currentColor">
+                                <animate
+                                    attributeName="fill-opacity"
+                                    begin="0s"
+                                    dur="1.3s"
+                                    values="0;0;0;0;0;0;0;1"
+                                    calcMode="linear"
+                                    repeatCount="indefinite"
+                                />
+                            </circle>
+                        </g>
                     </g>
                 </svg>
             </span>
